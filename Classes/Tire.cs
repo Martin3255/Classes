d67270d184c41cf4db600ba5c5e58913257f99f8
Martin3255 <martooo03@abv.bg>
2023-02-06 14:30:37 +0200

    Add project files.

---
+++ Classes/Tire.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        double pressure;
+
+        public int Year { get { return year; } set { year = value; } }
+        public double Pressure { get { return pressure; } set { pressure = value; } }
+
+        public Tire(int year, double pressure)  
+        {
+            this.year = year;
+            this.pressure = pressure;
+        }
+    }
+}
